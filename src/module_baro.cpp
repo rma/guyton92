601f5c84dd34fd0481dc1d16fb02ebc83eff677d
Rob Moss <rob.rgm@gmail.com>
2010-11-03 13:35:30 +0100

    Implemented the baroreceptor adaptation block as a separate module.
    Only the modular build of the model is affected.

---
+++ src/module_baro.cpp
@@ -0,0 +1,28 @@
+/**
+ * The baroreceptor adaptation module of the Guyton 1992 model.
+ */
+
+#include <cmath>    /* for pow(), fabs() */
+
+#include "params.h"
+#include "vars.h"
+#include "utils.h"
+#include "module_baro.h"
+
+/**
+ * This function calculates the adaptation of the baroreceptors.
+ *
+ * @param[in] p      The struct of model parameters.
+ * @param[in] v      The struct of state variables.
+ */
+void module_baro(const PARAMS &p, const VARS &v) {
+  *v.au2 = *v.au6 - 1;
+  *v.au8 = *p.auk * *v.au2;
+  *v.au4 = *v.au4 + *v.au8 * *v.i;
+
+  *v.dhm = (*v.pot - 10) * *p.dhdtr;
+  *v.hmd = *v.hmd + *v.dhm * *v.i;
+  if (*v.hmd > 1) {
+    *v.hmd = 1;
+  }
+}
---
+++ src/module_baro.h
@@ -0,0 +1,1 @@
+void module_baro(const PARAMS &p, const VARS &v);
