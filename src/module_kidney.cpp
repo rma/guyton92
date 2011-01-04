a9a03ed93ec5866ea45cdc1800add3b1e59e4826
Rob Moss <rob.rgm@gmail.com>
2011-01-04 22:09:07 +0100

    Extend the replacement renal module to simulate the distal segments.
    Fractional filtration constants are defined for Na+, K+ and H2O, for the
    early and late distal tubule segments and for the cortical, outer-medullary
    and inner-medullary collecting ducts.

--- src/module_kidney.cpp
+++ src/module_kidney.cpp
@@ -54,21 +54,58 @@ void module_kidney(const PARAMS &p, VARS &v) {
   solve_moore94_model(p, v);
 
   /* A linear regression was used to estimate MDFLW from Qalh. */
-  double mdflw = -3374.8165 + 1978.1256 * v.Qalh \
+  v.mdflw = -3374.8165 + 1978.1256 * v.Qalh \
     - 386.4192 * v.Qalh * v.Qalh + 25.1648 * v.Qalh * v.Qalh * v.Qalh;
 
+   /* Scale values to the human body (~2x10^6 nephrons) so that the mean RBF
+      is equivalent to the mean RBF as calculated by the original module. */
+  double scale = 2e6 * 3.531067;
+
   /* Renal blood flow. */
-  double rbf = 1e-9 * (v.Pas - v.Pg0) / v.Ra; /* L/min */
-  rbf *= p.rek; /* Account for the functional mass. */
+  v.rbf = 1e-9 * (v.Pas - v.Pg0) / v.Ra; /* L/min */
+  v.rbf *= p.rek; /* Account for the functional mass. */
+  v.rbf *= scale;
 
   /* Distal delivery of water, sodium and potassium. */
-  double distal_H2O = 1e-9 * v.Qalh; /* L/min */
-  double distal_Na = v.Ci; /* mM */
-  double distal_Na_rate = distal_H2O * distal_Na; /* mM/min */
-  double distal_K = distal_Na * v.cke / v.cna; /* mM */
-  double distal_K_rate = distal_H2O * distal_K; /* mM/min */
+  double EDCT_in_vol = scale * 1e-9 * v.Qalh; /* L/min */
+  double EDCT_in_Na = EDCT_in_vol * v.Ci; /* mmol/min */
+  double EDCT_in_K = EDCT_in_vol * v.Ci * v.cke / v.cna; /* mmol/min */
 
-  /* TODO: Solve models of the distal tubule and collecting duct. */
+  /* Fractional reabsorption of volume, sodium and potassium, in the early
+     distal convoluted tubule (EDCT), the connecting tubule (CNT), and the
+     cortical, outer-medullary and inner-medullary collecting ducts (CCD,
+     OMCD and IMCD, respectively). */
 
-  /* TODO: Return KOD and NOD (mEq/min), VUD and RBF (L/min) and MDFLW. */
+  /* EDCT fractional reabsorption from DOI: 10.1152/ajprenal.00043.2005
+     http://dx.doi.org/10.1152/ajprenal.00043.2005 */
+  double EDCT_reab_Na = 0.40;
+  double EDCT_reab_K = -0.86;
+  double EDCT_reab_vol = 0.10;
+  /* CNT fractional reabsorption from DOI: 10.1152/ajprenal.00044.2005
+     http://dx.doi.org/10.1152/ajprenal.00044.2005 */
+  double CNT_reab_Na = 0.36;
+  double CNT_reab_K = -5.60;
+  double CNT_reab_vol = 0.42;
+  /* CCD fractional reabsorption from AJP Renal 280:F1072-F1092, 2001. */
+  double CCD_reab_Na = 0.19;
+  double CCD_reab_K = 0.06;
+  double CCD_reab_vol = 0.63;
+  /* OMCD fractional reabsorption from AJP Renal 279:F24-F45, 2000. */
+  double OMCD_reab_Na = -0.25;
+  double OMCD_reab_K = 0.42;
+  double OMCD_reab_vol = 0.54;
+  /* IMCD fractional reabsorption from AJP Renal 274:F841-F855, 1998. */
+  double IMCD_reab_Na = 0.71;
+  double IMCD_reab_K = 0.65;
+  double IMCD_reab_vol = 0.77;
+
+  /* Calculate excretions of: Na (mEq/min), K (mEq/min) and volume (L/min). */
+  v.nod = EDCT_in_Na * (1 - EDCT_reab_Na) * (1 - CNT_reab_Na)
+    * (1 - CCD_reab_Na) * (1 - OMCD_reab_Na) * (1 - IMCD_reab_Na);
+  v.kod = EDCT_in_K * (1 - EDCT_reab_K) * (1 - CNT_reab_K)
+    * (1 - CCD_reab_K) * (1 - OMCD_reab_K) * (1 - IMCD_reab_K);
+  v.vud = EDCT_in_vol * (1 - EDCT_reab_vol) * (1 - CNT_reab_vol)
+    * (1 - CCD_reab_vol) * (1 - OMCD_reab_vol) * (1 - IMCD_reab_vol);
+
+  /* TODO: calculate the new plasma concentrations of Na and K? */
 }
