0a58ab68c65da4cbc269ddfe2686e3c63951b279
Rob Moss <rob.rgm@gmail.com>
2011-01-19 13:27:35 +0100

    Filters and outputs can now be controlled via the command line.
    The main() function now uses getopt to process command line arguments, so
    that filtering can be removed and the output variables can be specified.
    Usage instructions are also provided.

--- src/guyton92.cpp
+++ src/guyton92.cpp
@@ -14,7 +14,10 @@
 #include <cstdlib>
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <queue>
+#include <vector>
+#include <getopt.h>
 
 using namespace std;
 
@@ -38,6 +41,27 @@ using namespace std;
 #include "instr_vars.h"
 
 /**
+ * Displays the command-line usage for the model, then exits.
+ *
+ * @param progname The name of the program.
+ * @param exitcode The exit code to pass to exit().
+ */
+void usage(char* progname, int exitcode) {
+  cerr << "\n  USAGE: " << progname << " [options] [parameter file]" << endl;
+  cerr << "\n  OPTIONS:" << endl;
+  cerr << "    -a, --no-filter     " <<
+    "Display the output variables after each time-step." << endl;
+  cerr << "    -o, --outputs=VARS  " <<
+    "Set the output variables (comma-separated list)." << endl;
+  cerr << "    -h, --help          " <<
+    "Display this help and exit." << endl;
+  cerr << "\n  EXAMPLE:" << endl;
+  cerr << "    " << progname << " -a -o pa,rbf,vud ./exps/hypertension.exp\n";
+  cerr << endl;
+  exit(exitcode);
+}
+
+/**
  * The entry point for the modular Guyton 1992 model.
  *
  * @param[in] argc The number of arguments on the command line.
@@ -54,23 +78,83 @@ int main(int argc, char *argv[]) {
   /* Initialise the VARS struct (V). */
   VARS_INIT(v);
 
+  /* Options that can be set by command-line parameters. */
+  bool use_filter = true; /* Whether or not to filter notifications. */
+  bool use_outs = false; /* Whether output variables have been specified. */
+  vector<string> outs; /* The specified output variables. */
+
+  static struct option long_options[] = {
+    /* Options are distinguished by a single character. */
+    {"no-filter", no_argument,       0, 'a'},
+    {"outputs",   required_argument, 0, 'o'},
+    {"help",      no_argument,       0, 'h'},
+    {NULL, NULL, NULL, NULL}
+    };
+  int option_index = 0;
+  int c;
+
+  /* Process every parameter that has been given. */
+  while (1) {
+    c = getopt_long(argc, argv, "hao:", long_options, &option_index);
+    if (c == -1) {
+      break; /* No more parameters to process. */
+    }
+
+    /* Local variables for processing the list of output variables. */
+    istringstream ss;
+    string outname;
+
+    switch (c) {
+    case 'a':
+      /* Don't filter the notifications of the model state. */
+      use_filter = false;
+      break;
+    case 'o':
+      /* Use the output variables specified on the command line. */
+      use_outs = true;
+      ss.str(optarg);
+      while (getline(ss, outname, ',')) {
+        outs.push_back(outname);
+      }
+      break;
+    case 'h':
+      /* Print the usage information. */
+      usage(argv[0], EXIT_SUCCESS);
+      break;
+    case ':':
+    case '?':
+    default:
+      /* Incorrect usage, print the usage information. */
+      usage(argv[0], EXIT_FAILURE);
+      break;
+    }
+  }
+
+  /* Shift argc and argv so that the processed parameters are ignored. */
+  argc -= optind;
+  argv += optind;
+
+  /* Allow an experiment definition to be provided in an external file. */
   Experiment *e = NULL;
   ifstream input;
-
-  /* Allow parameter values to be defined in an external file. */
   switch (argc) {
-  case 1:
-    /* No arguments, only the command name. */
+  case 0:
+    /* No experiment definition. */
     break;
-  case 2:
+  case 1:
     /* One argument, which specifies the experiment file. */
-    input.open(argv[1]);
-    e = new Experiment(p, input);
+    input.open(argv[0]);
+    if (input.fail()) {
+      cerr << "ERROR: Unable to open experiment: '" << argv[0] << "'" << endl;
+      exit(EXIT_FAILURE);
+    } else {
+      e = new Experiment(p, input);
+    }
     input.close();
     break;
   default:
-    cerr << "USAGE: " << argv[0] << " [parameter file]" << endl;
-    return EXIT_FAILURE;
+    /* Incorrect usage, print the usage information. */
+    usage(argv[-optind], EXIT_FAILURE);
   }
 
   /* The simulation begins at time t = 0. */
@@ -85,12 +169,15 @@ int main(int argc, char *argv[]) {
   }
 
   /* Filter the notifications. */
-  const double *output_times = (e) ? e->output_times() : NULL;
-  add_filter(filter_times, (void *) output_times);
+  const double *output_times = NULL;
+  if (use_filter) {
+    output_times = (e) ? e->output_times() : NULL;
+    add_filter(filter_times, (void *) output_times);
+  }
   /* Display the specified model outputs. */
-  const vector<string> *outputs = (e) ? &e->output_vars() : NULL;
-  void* opts = instr_vars_opts(NULL, outputs);
-  add_instrument(instr_vars, opts);
+  vector<string> const *outputs =
+    (use_outs) ? &outs : (e) ? &e->output_vars() : NULL;
+  add_instrument(instr_vars, instr_vars_opts(NULL, outputs));
 
   /* Notify all registered instruments of the initial model state. */
   notify_instruments(p, v);
