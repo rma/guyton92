f7f740e75fdb9b241f8665f5b80a0c4f72f8e4cb
Rob Moss <rob.rgm@gmail.com>
2011-09-09 12:32:56 +0200

    Added a script for running any number of experiments.

---
+++ run.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+#
+# Run one or more experiments and record the results.
+#
+
+USAGE="USAGE: `basename ${0}` experiment_file(s)"
+
+if [ "$#" -lt "1" ]; then
+    echo ${USAGE}
+    exit 2
+fi
+
+MODEL="./build/guyton92"
+
+for EXP_FILE in "$@"; do
+    EXP_BASE="$(basename ${EXP_FILE} .exp)"
+    EXP_NAME=$(echo ${EXP_BASE} | tr [:lower:] [:upper:] | tr -d -c [:alnum:])
+    OUT_FILE="$(dirname ${EXP_FILE})/${EXP_BASE}.out"
+
+    if [ ! -r "${EXP_FILE}" ]; then
+        echo "ERROR: unable to read experiment '${EXP_FILE}'"
+        exit 2
+    fi
+
+    echo ${EXP_BASE}
+
+    make clobber
+    make model EXPERIMENT="${EXP_NAME}" ECHO=":"
+    ./build/guyton92 ${EXP_FILE} | awk 'i{print}/end-exp/{i++}' > ${OUT_FILE}
+done
